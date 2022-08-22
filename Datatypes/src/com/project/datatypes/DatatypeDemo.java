503818051270a6c217a8ab58862041a608dc65de
user <user@DESKTOP-7FNJ8MC>
2022-08-22 15:48:26 +0530

    Push

---
+++ Datatypes/src/com/project/datatypes/DatatypeDemo.java
@@ -0,0 +1,37 @@
+package com.project.datatypes;
+
+public class DatatypeDemo {
+	public static void main(String [] args) {//main method
+		int age=18;//from line no 5 to 11,,these are all primitve datatypes
+		boolean result=false;
+		byte number=-126;
+		char abc='G';
+		long cantactNumber=8548078524L;
+		float salary=25000.34764747f;
+		double price=23242.454;
+		String name="Infoysis";//it is a non-primitve datatype
+		//this String datatype should accept All types of primitve data
+		String age1="18";
+		String result1="True";
+		String abc1="G";
+		String cantactNumber1="8548078534";
+		String salary1="2435345.6566";
+		String price1="45432.56675654645";
+		
+		System.out.println("The value of Age is:"+age);
+		System.out.println("The value of Boolean is: "+result);
+		System.out.println("The value of Byte is: "+number);
+		System.out.println("The value of char is: "+abc);//shortcut key is:Syso then Ctrl+Space bar
+		System.out.println("The value of Long is:"+cantactNumber);
+		System.out.println("The value of Float is:"+salary);
+		System.out.println("The value of Double is : "+price);
+		System.out.println("The value of String is: "+name);
+		System.out.println(age1);
+		System.out.println(result1);
+		System.out.println(abc1);
+		System.out.println(cantactNumber1);
+		System.out.println(salary1);
+		System.out.println(price1);
+	}
+
+}
